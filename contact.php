4e2ecd8a5d38b4da8be73e2ab2b99bebb7a0e87f
Hindra Estrada <hindrave@gmail.com>
2022-08-31 00:07:06 -0500

    remove vendor form contact

--- contact.php
+++ contact.php
@@ -1,12 +1,13 @@
 <?php
 if(isset($_POST['submit'])){
-    $to = "hindrave@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
+    $to = "hindrave@gmail.com"
+    $from = $_POST['email'];
     $name = $_POST['name'];
     $subject = $_POST['text'];
     $message = $_POST['message'];
 
     $headers = "From:" . $from;
     mail($to,$subject,$message,$headers);
+    header("Location: index.html");
     }
 ?>
