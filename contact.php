0f38f990fc9a5521092562246faa9a231b388676
Hindra Estrada <hindrave@gmail.com>
2022-08-30 23:54:13 -0500

    modify contact.php file

--- contact.php
+++ contact.php
@@ -5,18 +5,7 @@
     $email_from = $_POST['email']; // required
     $subject = $_POST['text']; // required
     $message = $_POST['message']; // required
-
-   //Body message
-   $email_message = "Content msg.\n\n";
-   function clean_string($string) {
-     $bad = array("content-type","bcc:","to:","cc:","href");
-     return str_replace($bad,"",$string);
-   }
-
-   $email_message .= "Name: ".clean_string($name)."\n";
-   $email_message .= "Email: ".clean_string($email_from)."\n";
-   $email_message = "Subject: ".clean_string($subject)."\n";
-   $email_message .= "Message: ".clean_string($message)."\n";
+    
     //Creating mail headers
     $headers = 'From: '.$email_from."\r\n".
     'Reply-To: '.$email_from."\r\n" .
