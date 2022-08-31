c084c15bb0b5480d295289876532f92cbf7b2b5e
Hindra Estrada <hindrave@gmail.com>
2022-08-31 00:15:05 -0500

    add action to form

--- contact.php
+++ contact.php
@@ -8,13 +8,10 @@ $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
-$message = "Saludos Staff de Lucsax. \n\n Mi nombre es:  " . $name . ", los contacto a través de su plataforma en línea lucsax.net. \r\n\n";
-$message .= $_POST['msg'] . " \r\n\n";
-$message .= "\n Date message: " . date('d/m/Y', time());
 
 $to = 'hindrave@gmail.com';
 
-mail($to, $subject, utf8_decode($message), $header);
+mail($to, $subject, utf8_decode($msg), $header);
 
 header("Location:index.html");
 ?>
