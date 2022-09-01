0ee17fa3c8a00dd2b8eb58d1b4a352a0000a69f7
Hindra Estrada <hindrave@gmail.com>
2022-08-31 21:48:20 -0500

    add vendor autoload.php and some other php features

--- contact.php
+++ contact.php
@@ -1,17 +1,46 @@
 <?php
-$name = $_POST['name'];
-$mail = $_POST['email'];
-$subject = $_POST['subject'];
-$msg = $_POST['message'];
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+//Load Composer's autoloader
+require 'vendor/autoload.php';
 
-$to = 'hindrave@gmail.com';
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-mail($to, $subject, utf8_decode($msg), $header);
+try {
+    // Variables
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $subject = $_POST['subject'];
 
-header("location:index.html");
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    // host is outlook
+    $mail->Host       = 'mail.privateemail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'info@fixingbrokenhouses.com';                     //SMTP username
+    $mail->Password   = 'fbh2022';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail -> setFrom($email, $name);
+    $mail-> addReplyTo($email, $name);
+
+    //Content
+    $mail->isHTML(false);                                  //Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = $message;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 ?>
